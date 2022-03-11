9a2cbb2dfbfd12f3144754eef29691c98b2fd7e3
34ka <60933524+34ka@users.noreply.github.com>
2022-03-12 04:32:17 +0600

    Java tychs 3 git branch 2 (#1)
    * init commit
    
    * added assert false test example

---
+++ src/test/java/tests/SimpleTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+
+    @Test
+    public void assertTrueTest() {
+        assertTrue(3 > 2);
+    }
+
+    @Test
+    public void assertFalseTest() {
+        assertFalse(3 < 2);
+    }
+}
