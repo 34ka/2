484d24dc347353784258c9b65b6b99e7ef4b59cb
34ka <34kacode@gmail.com>
2022-03-11 11:16:56 +0600

    dz

--- src/test/java/dz/SelenideWithListener.java
+++ src/test/java/dz/SelenideWithListener.java
@@ -3,7 +3,9 @@ package dz;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selectors.byLinkText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideWithListener {
@@ -15,6 +17,11 @@ public class SelenideWithListener {
         open("https://github.com");
 
         $("[data-test-selector=nav-search-input]").setValue("34ka/2").pressEnter();
+
+        $(byLinkText("34ka/2")).click();
+        $("#issues-tab").click();
+
+        $()
         sleep(10000);
 
     }
