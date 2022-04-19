37d00214c3f45b61e0d45aa1db3c0162ed7eb557
34ka <34kacode@gmail.com>
2022-04-20 02:12:31 +0600

    refactoring

--- src/test/java/tests/dz/DemowebshopTests.java
+++ src/test/java/tests/dz/DemowebshopTests.java
@@ -24,17 +24,17 @@ public class DemowebshopTests {
 
     @Test
     public void addToWishListWithCookieTest() {
-                given()
-                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                        .cookie("Nop.customer=d5876974-632b-4c1f-9cf5-9af97672204d;")
-                        .when()
-                        .body("addtocart_43.EnteredQuantity=1")
-                        .post("http://demowebshop.tricentis.com/addproducttocart/details/43/2")
-                        .then()
-                        .log().all()
-                        .statusCode(200)
-                        .body("success", is(true))
-                        .body("message", is("The product has been added to your <a href=\"/wishlist\">wishlist</a>"))
-                        .body("updatetopwishlistsectionhtml", not(empty()));
+        given()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .cookie("Nop.customer=d5876974-632b-4c1f-9cf5-9af97672204d;")
+                .when()
+                .body("addtocart_43.EnteredQuantity=1")
+                .post("http://demowebshop.tricentis.com/addproducttocart/details/43/2")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("success", is(true))
+                .body("message", is("The product has been added to your <a href=\"/wishlist\">wishlist</a>"))
+                .body("updatetopwishlistsectionhtml", not(empty()));
     }
 }
